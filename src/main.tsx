a33383c23da25378e9671981583b47a50f315184
saytbork <juanamisano@gmail.com>
2025-11-29 12:42:16 -0600

    Handle Firebase email-link login on app load and redirect to dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 import './index.css';
+import { handleMagicLinkLogin } from './auth/handleMagicLink';
+
+handleMagicLinkLogin().catch(console.error);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
