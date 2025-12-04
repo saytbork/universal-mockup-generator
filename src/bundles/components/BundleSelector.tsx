930de12b859c953c7710bfcdec2aa29af4ff18b8
saytbork <juanamisano@gmail.com>
2025-12-04 13:49:02 -0600

    Remove bundle debug logs

--- src/bundles/components/BundleSelector.tsx
+++ src/bundles/components/BundleSelector.tsx
@@ -37,7 +37,6 @@ const BundleSelector: React.FC<BundleSelectorProps> = ({
     onGenerate(visibleProducts);
   };
 
-  console.log('DEBUG productMediaLibrary:', productMediaLibrary);
   return (
     <div className="space-y-4">
       <div className="flex flex-col gap-2">
@@ -68,7 +67,6 @@ const BundleSelector: React.FC<BundleSelectorProps> = ({
           <div className="flex flex-wrap gap-3">
             {slotProducts.map(slot => {
               const productMeta = slot.meta;
-              console.log('DEBUG productMeta:', productMeta);
               return (
                 <div key={slot.id} className="w-28 text-center text-xs text-gray-300">
                   <div className="relative h-28 w-full overflow-hidden rounded-xl border border-white/10 bg-black/20">
