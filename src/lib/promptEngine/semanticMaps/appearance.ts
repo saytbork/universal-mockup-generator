691f95d847852129bd85848254f09349c4cf6535
juan amisano <juanamisano@gmail.com>
2025-12-18 05:11:47 -0600

    chore: centralize appearance semantic map

---
+++ src/lib/promptEngine/semanticMaps/appearance.ts
@@ -0,0 +1,7 @@
+export const APPEARANCE_SEMANTIC_MAP: Record<string, string> = {
+    'Regular': 'regular everyday professional appearance',
+    'Well-Groomed': 'well-groomed but authentic professional appearance',
+    'Styled': 'styled but believable appearance, intentionally prepared',
+    'Messy / Just Woke Up': 'slightly messy, just woke up look with natural dishevelment',
+    'Running Late': 'running-late appearance with minor imperfections in hair and clothing'
+};
